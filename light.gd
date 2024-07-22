b4025492706ec7a3bc90776fe99ce9a8f5ecb794
krisk <krisk@krisks-MacBook-Pro.local>
2024-07-21 20:19:45 -0700

    trying something else so saving code

--- light.gd
+++ light.gd
@@ -5,13 +5,21 @@ extends Node2D
 func _ready():
 	update_light()
 	
+var lighted = []
 func update_light():
+	lighted = []
 	for i in $emitters.get_children():
-		var ray : RayCast2D = i.get_node("RayCast2D")
+		var cur = i.get_node("RayCast2D").global_position
+		var dir = i.get_node("RayCast2D").target_position.normalized()
 		for j in 256/16+1: # change eventually
-			ray.target_position += ray.target_position.normalized()*16
-
+			cur += dir*16
+			lighted.append(cur)
+	queue_redraw()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _draw():
+	for i in lighted:
+		draw_rect(Rect2(i - Vector2.ONE*8, Vector2.ONE*16), Color.WHITE)
