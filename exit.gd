a1eea24bd59ba1746a3bb68d0366891fbf516b80
krisk <krisk@krisks-MacBook-Pro.local>
2024-07-30 22:20:38 -0700

    qol stuff

--- exit.gd
+++ exit.gd
@@ -7,6 +7,7 @@ func _on_area_2d_body_entered(body):
 		Global.CURRENT_LEVEL += 1
 		get_tree().change_scene_to_file("res://levels/level%d.tscn" % Global.CURRENT_LEVEL)
 
-
-func _on_area_2d_2_body_entered(body):
-	$CollisionShape2D.set_deferred("disabled", body is Player and recivers.reduce(func(a, b): return a and b.activated, true))
+func _physics_process(delta):
+	var open = recivers.reduce(func(a, b): return a and b.activated, true)
+	$CollisionShape2D.set_deferred("disabled", open)
+	$Icon.visible = not open
